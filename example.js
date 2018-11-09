bc7edc12394140d3103d4b4c48b0fda384553592
Jimmy Hu <jim2212001@gmail.com>
2018-11-09 11:06:11 +0800

    Get Authorization after manually pass captcha

--- example.js
+++ example.js
@@ -1,30 +1,24 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-	  const browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false});
-	  const page = await browser.newPage();
-	  await page.setViewport({width: 1600, height: 900});
-	  await page.goto('https://cobinhood.com');
-	  console.log('Connected');
-	  await page.keyboard.press('Escape');
-	  await page.click('#desktop-header-login');
-	  await page.waitFor(3000);
-	  recaptcha = getRecaptcha(page.mainFrame());
-	  function getRecaptcha(frame) {
-        for (let child of frame.childFrames()) {
-					  if (child.url().startsWith('https://www.google.com/recaptcha/api2/anchor')) {
-							  return child;
-						}
-				}
-		}
-	  console.log('Recaptcha URL:' + recaptcha.url());
-    var checkbox = '.recaptcha-checkbox-checkmark';
-	  await recaptcha.click(checkbox);
-	  console.log('Clicked');
-	  await page.waitFor(5000);
-	  console.log('Done');
-	  await page.screenshot({path: 'example.png'});
-	  console.log('Close');
+	const browser = await puppeteer.launch({headless: false});
+	const page = await browser.newPage();
+	await page.setViewport({width: 1600, height: 900});
+	await page.goto('https://cobinhood.com');
+	console.log('Connected');
+	await page.keyboard.press('Escape');
+	await page.click('#desktop-header-login');
+	await page.waitForNavigation({timeout: 0});
+	const cookies = await page.cookies();
+	await browser.close();
+  const token = getToken(cookies);
 
-	  await browser.close();
+	console.log(token);
+	function getToken(cookies) {
+		for (let cookie of cookies) {
+			if (cookie.name == 'Authorization') {
+				return cookie.value;
+			}
+		}
+	}
 })();
